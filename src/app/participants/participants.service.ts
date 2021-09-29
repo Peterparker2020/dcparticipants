fcd9737165ea1c0e2485b1ada708a0e606a844c1
Peterparker2020 <gorzekperoramas@gmail.com>
2021-09-29 15:57:38 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,15 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "bot yolk", age:1 + " years old", gender: "Female"},
+            {name: "jay cap", age:313 + " years old", gender: "Male"},
+            {name: "chris tian", age:32 + " years old", gender: "Female"},
+            {name: "gwapo ko", age:35 + " years old", gender: "Male"},
+            {name: "dina karla", age:24 + " years old", gender: "Male"},
+            {name: "taguines judita", age:21 + " years old", gender: "Male"},
+            {name: "johny sin", age:11 + " years old", gender: "Male"},
+            {name: "mia ka", age:12 + " years old", gender: "Female"},
+        ];
+    }
+
+}
